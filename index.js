51cbdcffd94086ab213c5d035ae9e95cf6c0491d
akister142 <133674087+akister142@users.noreply.github.com>
2024-09-05 01:07:12 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { AsyncLocalStorage } = require("async_hooks");
+
+let anoAtual = 2024;
+const nomeCompleto = 'Matheus Aliski'
+let idade = 17;
+const cursoFaculdade = 'me matar'
+let cidadeAtual = 'SJP';
+
+const.log('Ola eu sou o '${nomeCompleto} 'eu tenho '$(idade) "moro em" ${cidadeAtual}; quero
+console.log(anoAtual);
+
+anoAtual = 2025;
+
+console.log(anoAtual);
+
