0a5bb78d452afdecbda21bbaae79749191402550
akister142 <133674087+akister142@users.noreply.github.com>
2024-09-26 01:33:55 +0000

    25/09

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { AsyncLocalStorage } = require("async_hooks");
+const prompt = require('prompt-sync');
+const entrada = prompt();
 
 let anoAtual = 2024;
 const nomeCompleto = 'Matheus Aliski'
@@ -6,10 +7,38 @@ let idade = 17;
 const cursoFaculdade = 'me matar'
 let cidadeAtual = 'SJP';
 
-const.log('Ola eu sou o '${nomeCompleto} 'eu tenho '$(idade) "moro em" ${cidadeAtual}; quero
+console.log(`Ola eu sou o '${nomeCompleto} 'eu tenho '$(idade) "moro em" ${cidadeAtual}; quero`);
 console.log(anoAtual);
 
 anoAtual = 2025;
 
 console.log(anoAtual);
 
+//agencia de viagens 
+
+var nomeComprador = entrada('qual seu nome?');
+var cpf = entrada('Qual o seu cpf?');
+var idadeComprador = entrada('Qual é a sua idade?');
+const listaDeDestinos = new  Array(
+  "Joinville",
+  " Florianṕolis",
+  " João Pessoa",
+  " Osasco",
+  " Lages",
+);
+
+console.log (`Olá senhor ${nomeComprador}, seu cpf é ${cpf} e idade ${idadeComprador}, certo? Sua viagem está reservada, com os seguintes destinos planejados: ${listaDeDestinos}´);
+
+console.log (listaDeDestinos[2]);
+console.log("");
+  
+console.log(´Boa noite robson, nós temos um novo destino adicionado à sua viagem`);
+
+listaDeDestinos.push("Rio de Janeiro");
+
+console.log(listaDeDestinos);
+
+
+if(idadeComprador<18){
+  console.log(`infelizmente nao podemos te vender uma passagem pois o senhor (a) é menor de idade`)
+}
